4b175283fb0a17bc50a599963c65f46fbf0eeda6
KhanhHG <honggiakhanh@gmail.com>
2024-03-13 18:48:04 +0200

    fix: unstable cache results in old cached data

--- src/lib/server-utils.ts
+++ src/lib/server-utils.ts
@@ -5,30 +5,28 @@ import { notFound } from "next/navigation";
 import prisma from "./db";
 import { capitalize } from "./utils";
 
-export const getEvents = unstable_cache(
-  async (city: string, page: number = 1) => {
-    const events = await prisma.eventoEvent.findMany({
-      where: {
-        city: city === "all" ? undefined : capitalize(city),
-      },
-      orderBy: {
-        date: "asc",
-      },
-      take: 6,
-      skip: (page - 1) * 6,
-    });
+export const getEvents = async (city: string, page: number = 1) => {
+  const events = await prisma.eventoEvent.findMany({
+    where: {
+      city: city === "all" ? undefined : capitalize(city),
+    },
+    orderBy: {
+      date: "asc",
+    },
+    take: 6,
+    skip: (page - 1) * 6,
+  });
 
-    const totalCount = await prisma.eventoEvent.count({
-      where: {
-        city: city === "all" ? undefined : capitalize(city),
-      },
-    });
+  const totalCount = await prisma.eventoEvent.count({
+    where: {
+      city: city === "all" ? undefined : capitalize(city),
+    },
+  });
 
-    return { events, totalCount };
-  }
-);
+  return { events, totalCount };
+};
 
-export const getEvent = unstable_cache(async (slug: string) => {
+export const getEvent = async (slug: string) => {
   const event = await prisma.eventoEvent.findUnique({
     where: {
       slug: slug,
@@ -40,4 +38,4 @@ export const getEvent = unstable_cache(async (slug: string) => {
   }
 
   return event;
-});
+};
